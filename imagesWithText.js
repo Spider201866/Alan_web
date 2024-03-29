c99ec27e8b1fad5c4b862f80b127b624b94eb501
Your Name <you@example.com>
2024-03-29 16:34:54 +0000

    1634imageandtext

---
+++ imagesWithText.js
@@ -0,0 +1,19 @@
+// imagesWithText.js
+export const imagesWithText = [
+  {
+    imageName: 'allergic_conjunctivitis.jpg',
+    text: 'Allergic Conjunctivitis is characterized by redness, itching, and watering of the eyes.',
+    condition: 'Allergic Conjunctivitis'
+  },
+  {
+    imageName: 'iritis.jpg',
+    text: 'Iritis involves inflammation of the iris and can cause eye pain and photophobia. Says the butcher BBlaikie. And so on and more and more it goes on to make the line longer .',
+    condition: 'Iritis'
+  },
+  {
+    imageName: 'pingueculae.jpg',
+    text: 'Pingueculae are benign growths on the white part of the eye, often related to sun exposure.',
+    condition: 'Pingueculae'
+  },
+  // Add more objects as needed
+];
