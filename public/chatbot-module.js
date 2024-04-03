5e6a902e0fa5ae26b9cb5c9f821e319f34979685
Your Name <you@example.com>
2024-04-03 20:25:43 +0100

    2025

--- public/chatbot-module.js
+++ public/chatbot-module.js
@@ -1,5 +1,5 @@
 // chatbot-module.js
-import Chatbot from "https://cdn.jsdelivr.net/gh/Spider201866/FlowiseChatEmbed@v2.1/dist/web.js";
+import Chatbot from "https://cdn.jsdelivr.net/gh/Spider201866/FlowiseChatEmbed@v2.2/dist/web.js";
 
 const initChatbot = () => {
   Chatbot.initFull({
