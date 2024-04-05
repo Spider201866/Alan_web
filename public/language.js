b214ecca1527c0b973781d085faa23a6475a5390
Your Name <you@example.com>
2024-04-05 21:17:34 +0100

    2117

--- public/language.js
+++ public/language.js
@@ -3,33 +3,90 @@ export const translations = {
       "eyesEars": "Eyes & Ears",
       "goodHistory": "Good History",
       "examineWell": "Examine Well",
-      "useArclight": "Use Arclight", 
+      "useArclight": "Use Arclight",
       "howCanIHelp": "How can I help you today?",
       "alanMistakes": "*Alan can make mistakes. Use clinical judgement. 4/24"
     },
+    // Français
     fr: {
       "eyesEars": "Yeux & Oreilles",
       "goodHistory": "Bonne Anamnèse",
       "examineWell": "Bien Examiner",
-      "useArclight": "Utiliser Arclight", 
+      "useArclight": "Utiliser Arclight",
       "howCanIHelp": "Comment puis-je vous aider aujourd'hui ?",
       "alanMistakes": "*Alan peut se tromper. Utilisez votre jugement clinique. 4/24"
     },
+    // Español
     es: {
       "eyesEars": "Ojos & Oídos",
       "goodHistory": "Historia Clínica",
       "examineWell": "Examinar Cuidadosamente",
-      "useArclight": "Usar Arclight", 
+      "useArclight": "Usar Arclight",
       "howCanIHelp": "¿Cómo puedo ayudarte hoy?",
       "alanMistakes": "*Alan puede cometer errores. Usa el juicio clínico. 4/24"
     },
+    // Português  
     pt: {
       "eyesEars": "Olhos & Ouvidos",
       "goodHistory": "História Clínica",
       "examineWell": "Examinar Bem",
-      "useArclight": "Usar Arclight", 
+      "useArclight": "Usar Arclight",
       "howCanIHelp": "Como posso te ajudar hoje?",
       "alanMistakes": "*Alan pode cometer erros. Use o julgamento clínico. 4/24"
-    }
+    },
+    // Arabic
+    ar: {
+      "eyesEars": "العيون والأذنين",
+      "goodHistory": "تاريخ جيد",
+      "examineWell": "فحص جيد",
+      "useArclight": "استخدم Arclight",
+      "howCanIHelp": "كيف يمكنني مساعدتك اليوم؟",
+      "alanMistakes": "*آلان يمكن أن يخطئ. استخدم الحكم السريري. 4/24"
+    },
+    // Kiswahili/Swahili
+    sw: {
+      "eyesEars": "Macho na Masikio",
+      "goodHistory": "Historia Nzuri",
+      "examineWell": "Chunguza Vizuri",
+      "useArclight": "Tumia Arclight",
+      "howCanIHelp": "Ninawezaje kukusaidia leo?",
+      "alanMistakes": "*Alan anaweza kukosea. Tumia hukumu ya kliniki. 4/24"
+    },
+    // Chichewa (Malawi)
+    ny: {
+      "eyesEars": "Masomphenya & Makutu",
+      "goodHistory": "Mbiri Yabwino",
+      "examineWell": "Fufuzani Mwachidwi",
+      "useArclight": "Gwiritsani Ntchito Arclight",
+      "howCanIHelp": "Ndingakuthandizeni bwanji lero?",
+      "alanMistakes": "*Alan angalakwitse. Gwiritsani ntchito chiweruzo cha zamankhwala. 4/24"
+    },
+    // Kinyarwanda (Rwanda)
+    rw: {
+      "eyesEars": "Amaso n'Amatwi",
+      "goodHistory": "Amateka Meza",
+      "examineWell": "Suzuma Neza",
+      "useArclight": "Koresha Arclight",
+      "howCanIHelp": "Nakora iki kugufasha uyu munsi?",
+      "alanMistakes": "*Alan ashobora gukosa. Koresha ubushishozi bw'ubuvuzi. 4/24"
+    },
+    // Luganda (Uganda)
+    lg: {
+      "eyesEars": "Amaso n'Amatwi",
+      "goodHistory": "Ebyafaayo Ebirungi",
+      "examineWell": "Kebera Bulungi",
+      "useArclight": "Kozesa Arclight",
+      "howCanIHelp": "Nnyinza kukuyamba ntya leero?",
+      "alanMistakes": "*Alan ayinza okukola ensobi. Kozesa okusalirira okw'ekinnansi. 4/24"
+    },
+    // Cymraeg/Welsh
+    cy: {
+        "eyesEars": "Llygaid a Chlustiau",
+        "goodHistory": "Hanes Da",
+        "examineWell": "Archwilio'n Dda",
+        "useArclight": "Defnyddio Arclight",
+        "howCanIHelp": "Sut gallai helpu chi heddiw?",
+        "alanMistakes": "*Gall Alan wneud camgymeriadau. Defnyddiwch farn glinigol. 4/24"
+      }   
   };
   
