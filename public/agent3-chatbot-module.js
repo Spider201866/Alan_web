96d155b2164fda1bc3ded25d9206872885d3f3ad
Your Name <you@example.com>
2024-12-16 14:00:24 +0000

    legs11

--- public/agent3-chatbot-module.js
+++ public/agent3-chatbot-module.js
@@ -1,6 +1,52 @@
 // LLM3 [M && LI/VLI]
 
-Chatbot.initFull({
-  chatflowid: "db68b77b-92be-4c80-bf50-c13e00a33d77",
-  apiHost: "https://flowiseai-railway-production-fecf.up.railway.app",
-});
+import Chatbot from "https://cdn.jsdelivr.net/gh/Spider201866/FlowiseChatEmbed@geoff16/dist/web.js";
+
+const initChatbot = () => {
+;
+
+  // Initialise the chatbot with the retrieved name
+  Chatbot.initFull({
+    chatflowid: "db68b77b-92be-4c80-bf50-c13e00a33d77", // Your Flow ID
+    apiHost: "https://flowiseai-railway-production-fecf.up.railway.app", // Your API Host
+
+    theme: {
+      button: {
+        backgroundColor: "#ffffff",
+        right: 30,
+        bottom: 20,
+        size: "medium",
+        iconColor: "black",
+        customIconSrc:
+          "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg",
+      },
+      chatWindow: {
+        welcomeMessage: false,
+        backgroundColor: "#ffffff",
+        height: "auto",
+        width: "auto",
+        fontSize: 15,
+        poweredByTextColor: "#D3D3D3",
+        botMessage: {
+          backgroundColor: "#ffffff",
+          textColor: "#000000",
+          showAvatar: true,
+          avatarSrc: "https://raw.githubusercontent.com/Spider201866/Alan_web/main/public/black.png", // Local avatar URL
+        },
+        userMessage: {
+          backgroundColor: "#f4f4f4",
+          textColor: "#000000",
+          showAvatar: false,
+        },
+        textInput: {
+          placeholder: "Message Alan...",
+          backgroundColor: "#ffffff",
+          textColor: "#303235",
+          sendButtonColor: "#000000",
+        },
+      },
+    },
+  });
+};
+
+export { initChatbot };
